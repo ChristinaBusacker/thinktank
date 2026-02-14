cda92d5e31072ec78e76bfecb190a01fa0fba24c
Christina <email@christina-busacker.de>
2026-02-14 19:31:29 +0100

    fix aditional informations

--- src/app/pages/event/event.component.ts
+++ src/app/pages/event/event.component.ts
@@ -47,7 +47,7 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 export class EventComponent implements OnInit {
   event?: CMSObject;
   lang$: Observable<'de' | 'en'> = inject(Store).select(
-    LocalizationState.getLanguage
+    LocalizationState.getLanguage,
   );
   openedAccordion: string = 'venue';
 
@@ -61,7 +61,7 @@ export class EventComponent implements OnInit {
     @Inject(PLATFORM_ID) private platformId: Object,
     private seo: SeoService,
     public localizationService: LocalizationService,
-    private sanitizer: DomSanitizer
+    private sanitizer: DomSanitizer,
   ) {}
 
   public getEvent(): Event {
@@ -80,7 +80,7 @@ export class EventComponent implements OnInit {
         .select(CMSState.getEvents)
         .subscribe(
           (events) =>
-            (this.event = events.find((event) => event.data.url === slug))
+            (this.event = events.find((event) => event.data.url === slug)),
         );
 
       if (this.event) {
@@ -99,14 +99,16 @@ export class EventComponent implements OnInit {
         ]);
 
         this.transformedHtml = this.replaceYouTubeLinks(
-          this.event.data.text.html
+          this.event.data.text.html,
         );
 
         this.captionTransformed = this.ensureATagsToTargetBlank(
-          this.event.data.caption?.html || ''
+          this.event.data.caption?.html || '',
         );
       }
     });
+
+    console.log(this.event);
   }
 
   setOpenedAccordion(id: string) {
@@ -134,7 +136,7 @@ export class EventComponent implements OnInit {
         iframe.setAttribute('frameborder', '0');
         iframe.setAttribute(
           'allow',
-          'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture',
         );
         iframe.setAttribute('allowfullscreen', '');
         iframeWrapper.appendChild(iframe);
@@ -150,7 +152,7 @@ export class EventComponent implements OnInit {
         (
           _,
           __,
-          videoId
+          videoId,
         ) => `<div class="thumbnail-wrapper" data-video-id="${videoId}"><img src="https://img.youtube.com/vi/${videoId}/hqdefault.jpg" 
                   alt="YouTube Video Thumbnail" 
                   class="youtube-thumbnail" 
@@ -159,8 +161,8 @@ export class EventComponent implements OnInit {
                     <svg class="thumbnail-overlay-play" data-video-id="${videoId}" width="42px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="red"><path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM10.6219 8.41459C10.5562 8.37078 10.479 8.34741 10.4 8.34741C10.1791 8.34741 10 8.52649 10 8.74741V15.2526C10 15.3316 10.0234 15.4088 10.0672 15.4745C10.1897 15.6583 10.4381 15.708 10.6219 15.5854L15.5008 12.3328C15.5447 12.3035 15.5824 12.2658 15.6117 12.2219C15.7343 12.0381 15.6846 11.7897 15.5008 11.6672L10.6219 8.41459Z"/></svg>
                     <p data-video-id="${videoId}">Youtube Video laden</p>
                   </div>
-                </div>`
-      )
+                </div>`,
+      ),
     );
   }
 
